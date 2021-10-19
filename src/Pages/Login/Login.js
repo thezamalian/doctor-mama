c417130e83b82d66e8007a98b37bb927ed2252bc
Zamal Hossain <thezamalian@gmail.com>
2021-10-19 19:05:02 +0600

    Not Found page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className='text-center'>
             <h2>Please Login</h2>
-            <div className="form-signin mx-auto w-25">
+            <div className="form-signin mx-auto w-25 ">
                 <form>
-                    <div className="form-floating">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <div className="form-floating my-1">
+                        <input type="email" className="form-control" id="floatingInput" placeholder="" />
                         <label for="floatingInput">Email address</label>
                     </div>
-                    <div className="form-floating">
-                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <div className="form-floating my-1">
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="" />
                         <label for="floatingPassword">Password</label>
                     </div>
 
@@ -20,9 +21,11 @@ const Login = () => {
                             <input type="checkbox" value="remember-me" /> Remember me
                         </label>
                     </div>
+                    <h6 className='text-danger '>New Here, Please <Link to="/register" className='text-decoration-none'>Register</Link></h6>
+
                     <button className="w-100 btn btn-lg btn-primary" type="submit">Login</button>
 
-                    <button className="w-100 btn btn-lg btn-warning my-3 " type="submit">Login with Google</button>
+                    <button className="w-100 btn btn-lg btn-warning my-3 " type="submit">Continue with Google</button>
                 </form>
             </div>
         </div>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
+        <div className='text-center'>
             <h2>Please Register</h2>
+            <div className="form-signin mx-auto w-25 ">
+                <form>
+                    <div className="form-floating my-1">
+                        <input type="text" className="form-control" id="floatingName" placeholder="" />
+                        <label for="floatingName">Full Name</label>
+                    </div>
+                    <div className="form-floating my-1">
+                        <input type="email" className="form-control" id="floatingEmail" placeholder="" />
+                        <label for="floatingEmail">Email address</label>
+                    </div>
+                    <div className="form-floating my-1">
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="" />
+                        <label for="floatingPassword">Password</label>
+                    </div>
+                    <div className="form-floating my-1">
+                        <input type="password" className="form-control" id="floatingConfirmPassword" placeholder="" />
+                        <label for="floatingConfirmPassword">Confirm Password</label>
+                    </div>
+
+
+
+                    <div className="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me" /> Remember me
+                        </label>
+                    </div>
+                    <h6 className='text-danger '>Already a member, Please <Link to="/login" className='text-decoration-none'>Login</Link></h6>
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+
+                    <button className="w-100 btn btn-lg btn-warning my-3 " type="submit">Continue with Google</button>
+                </form>
+            </div>
         </div>
     );
 };
